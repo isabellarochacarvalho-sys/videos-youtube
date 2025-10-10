209c4743f22736f684b0654ce8d4cb4779777310
isabellarochacarvalho-sys <isabella.rocha.carvalho@escola.pr.gov.br>
2025-10-10 09:00:15 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const avanca = document. querySelectorAll('.btn-proximo');
+
+avanca.forEach(button =>)  {
+       button.addEventlistener('click , fuction(){
+        const atual = document.querySelector('.ativo');
+        const proximopasso = 'passo-' + this.getattribute('data-')
